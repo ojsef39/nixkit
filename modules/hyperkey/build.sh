b427f9eb79e47b52a2c50005f088a3c860338df6
daniel <daniel.inama02@gmail.com>
2025-04-25 23:06:54 +0200

    chore: redirect output

--- modules/hyperkey/build.sh
+++ modules/hyperkey/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 swiftc hyperkey.swift -framework Cocoa -framework Carbon
-codesign --entitlements hyperkey.entitlements -fs - hyperkey
+codesign --entitlements hyperkey.entitlements -fs - hyperkey 2> /dev/null
 #
 # hidutil property --set '{"UserKeyMapping":[]}'
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000006D}]}'
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000006D}]}' > /dev/null
